0fbf5c326e84e4ac20a281741ae85c9002610e36
Peter Möller <petmo338@gmail.com>
2022-02-07 23:18:45 +0100

    Works

---
+++ index.js
@@ -0,0 +1,84 @@
+const axios = require('axios')
+require('dotenv').config()
+
+const API_KEY = process.env.API_KEY
+
+const data = JSON.stringify({
+  query: `{
+    viewer {
+        homes { 
+        currentSubscription {
+          priceInfo {
+            today {
+              total
+              startsAt
+            }
+          } 
+        }
+      }
+    }
+  }`,
+  variables: {}
+})
+
+const config = {
+  method: 'post',
+  url: 'https://api.tibber.com/v1-beta/gql',
+  headers: {
+    Authorization: 'Bearer ' + API_KEY,
+    'Content-Type': 'application/json'
+  },
+  data: data
+}
+
+// axios(config)
+//   .then(function (response) {
+//     console.log(JSON.stringify(response.data));
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+async function getCurrentPrice () {
+  const response = await axios(config)
+  let prices = []
+  if (response.status < 300) {
+    prices = response.data.data.viewer.homes[0].currentSubscription.priceInfo.today
+    // console.log(JSON.parse(response.data))
+  } else {
+    console.log(response)
+  }
+  const currentTS = new Date()
+  for (const price of prices) {
+    const ts = new Date(price.startsAt)
+    if (ts - currentTS > -3600 * 1000) {
+      return price.total
+    }
+  }
+}
+(async () => {
+  try {
+    const price = await getCurrentPrice()
+    console.log('Current Price [SEK]: ' + price)
+    let garoConf
+    if (price < process.env.PRICE_LIMIT) {
+      garoConf = {
+        method: 'post',
+        url: 'http://' + process.env.GARO_IP + ':8080/servlet/rest/chargebox/mode/ALWAYS_ON'
+      }
+      console.log('Loading possible')
+    } else {
+      garoConf = {
+        method: 'post',
+        url: 'http://' + process.env.GARO_IP + ':8080/servlet/rest/chargebox/mode/ALWAYS_OFF'
+      }
+      console.log('Loading disabled')
+    }
+    const response = await axios(garoConf)
+    if (response.status !== 200) {
+      console.log(response.statusText)
+    }
+  } catch (e) {
+    // Deal with the fact the chain failed
+  }
+})()
